bb3df4ac565c82f1629b558190cf9d1d4f36e7e9
Nicolás <lieby2142@gmail.com>
2022-11-13 02:48:21 -0300

    Menu admin productos

--- Main/menuProductos.h
+++ Main/menuProductos.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "productos.h"
 /// ACA VAN LAS ESTRUCTURAS Y PROTOTIPADOS
 
 /// ESTRUCTURAS
@@ -8,3 +9,18 @@
 int menuProductos2(int id, int cursor);
 void mostrarOpcionesProductos2(int cantidadOpciones,opciones arreglo[], int cursor);
 void switchProductos(int id, int opcion);
+
+/// FUNCIONES MENU ADMIN PRODUCTOS
+void ingresarProductoAbuscar(char productoBuscado[50]);
+void buscarProducto(nodoProductoD* listaProductos,char productoBuscado[]);
+nodoProductoD* crearNodoDobleProducto(producto aux);
+nodoProductoD* despersistirListaDobleProductos(nodoProductoD* lista);
+nodoProductoD* agregarAlFinalDobleProducto(nodoProductoD*lista,nodoProductoD*aux2);
+void agregarProductoAdmin();
+void borrarPantalla();
+void buscarProductoFuncion();
+void cargarProductoNuevoFuncion();
+int seleccionarSiNoMenuProducto(int cursor); /// 1 ES SI 2 ES NO
+void mostrarSiNoMenuProducto(int cursor);
+
+
